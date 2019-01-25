27ac65009e25b9e28de7a85603e75fc0ebcba40f
Michael Sulyak <m.sulyak@gmail.com>
2019-01-25 23:10:49 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-split-testing/precache-manifest.28e63b89c490086c947c99d573dc1ebb.js"
+  "/react-split-testing/precache-manifest.8a4738c6b4b6bd30e70da494b07d3160.js"
 );
 
 workbox.clientsClaim();
