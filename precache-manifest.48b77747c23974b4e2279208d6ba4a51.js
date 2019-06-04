bb51897a4a8f4abd072a8eb44479d6529974b002
expert_m <m.sulyak@gmail.com>
2019-06-04 10:21:25 +0700

    Updates

---
+++ precache-manifest.48b77747c23974b4e2279208d6ba4a51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a7247c1f3506d9ebd574b6d655f2bc",
+    "url": "/react-split-testing/index.html"
+  },
+  {
+    "revision": "2fbe157394269021dec8",
+    "url": "/react-split-testing/static/css/main.b8fe9adb.chunk.css"
+  },
+  {
+    "revision": "b785a5e047b08a757288",
+    "url": "/react-split-testing/static/js/2.2f9c6bc1.chunk.js"
+  },
+  {
+    "revision": "2fbe157394269021dec8",
+    "url": "/react-split-testing/static/js/main.9a13fad4.chunk.js"
+  },
+  {
+    "revision": "17e076ac05995941adc9",
+    "url": "/react-split-testing/static/js/runtime~main.79c4a9b4.js"
+  }
+]);
